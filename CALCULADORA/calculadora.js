fc2cdff6125f3a01a94048e40173cbfe3fbd89e5
Mabe783 <mbacosta3@espe.edu.ec>
2025-06-04 16:04:45 -0500

    codigo java

--- CALCULADORA/calculadora.js
+++ CALCULADORA/calculadora.js
@@ -7,11 +7,67 @@ let isSecond = false;
 const input = document.getElementById('input');
 const opDisplay = document.getElementById('operation');
 const btnContainer = document.getElementById('buttons-numbers');
-// creamos botones numericos
-for (let i = 1; i <= 9; i++) {
-  const btn = document.createElement('button');
-  btn.className = 'btn btn-outline-primary';
-  btn.textContent = i;
-  btn.onclick = () => appendNumber(i);
-  btnContainer.appendChild(btn);
+
+
+
+function appendNumber(num) {
+  if (!isSecond) {
+    firstNumber += num;
+    input.value = firstNumber;
+  } else {
+    secondNumber += num;
+    input.value = secondNumber;
+  }
+}
+
+function chooseOperation(op) {
+  if (firstNumber === '') return;
+  operation = op;
+  isSecond = true;
+  input.value = '';
+  opDisplay.textContent = `Operación: ${operation}`;
+}
+
+function calculate() {
+  if (firstNumber === '' || secondNumber === '' || operation === '') return;
+  let result;
+  const num1 = parseFloat(firstNumber);
+  const num2 = parseFloat(secondNumber);
+
+  switch (operation) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      if (num2 === 0) {
+        alert("No se puede dividir entre cero");
+        return;
+      }
+      result = num1 / num2;
+      break;
+    default:
+      return;
+  }
+
+  input.value = result;
+  opDisplay.textContent = '';
+  firstNumber = result.toString();
+  secondNumber = '';
+  operation = '';
+  isSecond = false;
+}
+
+function clearAll() {
+  firstNumber = '';
+  secondNumber = '';
+  operation = '';
+  isSecond = false;
+  input.value = '';
+  opDisplay.textContent = '';
 }
